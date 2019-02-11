0a262dd4a4d0272543e19196175c7700dca2e6f9
AidasBaranauskas <44964790+AidasBaranauskas@users.noreply.github.com>
2019-02-11 12:26:48 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-//#include <fstream>
 #include <string>
 
 
@@ -11,12 +10,19 @@ int main()
     cout<<"iveskite megstamiausio mokytojo varda"<<endl;
     cin>>vardas;
     cout<<"iveskite remelio dydi"<<endl;
-    int x,y;
-    cin>>y;
-    if(y<0)y*=-1;
-    y-=(y+1)%2;
-    cout<<y<<endl;
-    pasisveik="* Sveikas, "+vardas+"! *";
+    int x,zy,y;//zy yra laikina reiksme
+    cin>>zy;
+    y=zy;
+    if(y<0)
+        y*=-1;//jeigu neigiamas???
+    if(y<3)y=3;//jeigu per mazas
+    if(y%2==0)y--;//uztikrinama simetrija
+    if(zy!=y)cout<<"duotas dydis netinkamas ir buvo pakeistas: "<<y<<endl;
+
+    if(vardas[vardas.size()-1]=='s')
+        pasisveik="* Sveikas, "+vardas+"! *";
+    else
+        pasisveik="* Sveika, "+vardas+"! *";
     x=pasisveik.size();
     tarpas="*";
     for(int i=1; i<x-1; i++)
@@ -26,13 +32,15 @@ int main()
     tarpas+="*";
 
     for(int i=0; i<x; i++)
-    krastas+="*";
+        krastas+="*";
 
     cout<<krastas<<endl;
-    for(int i=1;i<y-1;i++)
+    for(int i=1; i<y-1; i++)
     {
-        if(i==y/2)cout<<pasisveik<<endl;
-        else cout<<tarpas<<endl;
+        if(i==y/2)
+            cout<<pasisveik<<endl;
+        else
+            cout<<tarpas<<endl;
     }
     cout<<krastas<<endl;
     return 0;
